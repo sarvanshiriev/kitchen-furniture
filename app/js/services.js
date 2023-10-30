8133b433cbc2b6e25ed5b1e5dd6bfcf8b848022f
Sarvan Shiriev <sarvan.shiriev@mail.ru>
2023-10-30 17:17:25 +0500

    Реализована анимация появления

---
+++ app/js/services.js
@@ -0,0 +1,18 @@
+function activateAnimationOnScroll() {
+    var servicesWrapper = document.getElementById('servicesWrapper');
+    var items = servicesWrapper.querySelectorAll('.services__item');
+
+    function checkScroll() {
+        var rect = servicesWrapper.getBoundingClientRect();
+
+        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+            items.forEach(function(item, index) {
+                item.style.animation = `slideFromLeft 2s ease forwards ${index * 1}s`;
+            });
+        }
+    }
+    window.addEventListener('load', checkScroll);
+    window.addEventListener('scroll', checkScroll);
+}
+
+activateAnimationOnScroll();
